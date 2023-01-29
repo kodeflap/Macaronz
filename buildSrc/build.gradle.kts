4076abf1fd10bf20f13deacbbb60f45c9a3b4b12
Pooja shaji <poojashaji321@gmail.com>
2023-01-29 21:35:31 +0530

    updating project with dependency management

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) $originalComment.match("Copyright \(c\) (\d+)", 1, "-", "$today.year")[$today.year] [kodeflap]
+ * Copyright [2023] [kodeflap]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
