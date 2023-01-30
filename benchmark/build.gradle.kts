e6d5f45f5e176eb4a19c939997c9291faa317033
Pooja shaji <poojashaji321@gmail.com>
2023-01-30 11:58:35 +0530

    formatting code

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -17,53 +17,53 @@
 import com.kodeflap.sliderz.Configuration
 
 plugins {
-    id("com.android.test")
-    id("org.jetbrains.kotlin.android")
+  id("com.android.test")
+  id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.kodeflap.benchmark"
+  namespace = "com.kodeflap.benchmark"
   compileSdk = 31
 
   compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 
-    defaultConfig {
-        minSdk = 23
-        targetSdk = Configuration.targetSdk
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  defaultConfig {
+    minSdk = 23
+    targetSdk = Configuration.targetSdk
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        // This benchmark buildType is used for benchmarking, and should function like your
-        // release build (for example, with minification on). It"s signed with a debug key
-        // for easy local/CI testing.
-        create("benchmark") {
-            isDebuggable = true
-            signingConfig = getByName("debug").signingConfig
-            matchingFallbacks += listOf("release")
-        }
+  buildTypes {
+    // This benchmark buildType is used for benchmarking, and should function like your
+    // release build (for example, with minification on). It"s signed with a debug key
+    // for easy local/CI testing.
+    create("benchmark") {
+      isDebuggable = true
+      signingConfig = getByName("debug").signingConfig
+      matchingFallbacks += listOf("release")
     }
+  }
 
-    targetProjectPath = ":app"
-    experimentalProperties["android.experimental.self-instrumenting"] = true
+  targetProjectPath = ":app"
+  experimentalProperties["android.experimental.self-instrumenting"] = true
 }
 
 dependencies {
-    implementation(libs.androidx.test.runner)
-    implementation(libs.androidx.test.uiautomator)
-    implementation(libs.androidx.benchmark.macro)
-    implementation(libs.androidx.profileinstaller)
+  implementation(libs.androidx.test.runner)
+  implementation(libs.androidx.test.uiautomator)
+  implementation(libs.androidx.benchmark.macro)
+  implementation(libs.androidx.profileinstaller)
 }
 
 androidComponents {
-    beforeVariants(selector().all()) {
-        it.enable = it.buildType == "benchmark"
-    }
+  beforeVariants(selector().all()) {
+    it.enable = it.buildType == "benchmark"
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,23 +17,22 @@
 @file:Suppress("UnstableApiUsage")
 
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+    maven(url = "https://plugins.gradle.org/m2/")
+  }
 }
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
+  repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://plugins.gradle.org/m2/")
+  }
 }
 
 rootProject.name = "Sliderz"
 include(":app")
 include(":sliderz")
 include(":benchmark")
-
